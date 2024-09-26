d8148658b0c635934c6c7ff7378b49bd0cf80804
rodrigo gayoso <rodrigogm.dev@gmail.com>
2024-09-25 22:10:54 -0500

    se agregó un checkbox a las tareas nuevas

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 // Proyecto: Lista de Tareas
 ;(() => {
   const btnAddTask = document.getElementById('add-task')
-  btnAddTask.addEventListener('click', crearNota)
+  btnAddTask.addEventListener('click', crearTarea)
   setFocoNuevaTarea()
 })()
 
@@ -11,16 +11,14 @@ function setFocoNuevaTarea () {
   newTask.focus()
 }
 
-function crearNota () {
-  const nuevoDiv = document.createElement('div')
-  const nuevoTextArea = document.createElement('textarea')
-  const nuevoBtn = document.createElement('button')
-
+function crearTarea () {
   const style = document.createElement('style')
   const reglasCssDiv = `
   .nuevo-div {
       display: flex;
-      align-items: start;
+      align-items: center;
+      margin-bottom: 10px;
+      // border: 1px solid black;
     }
   `
   const reglasCssTextArea = `
@@ -32,32 +30,56 @@ function crearNota () {
       border-radius: 5px;
       border: 2px solid black;
       padding-inline: 2px;
-      margin-bottom: 10px;
       overflow: auto;
   }
   `
   const reglasCssBtn = `
   .list-btns {
     padding: 2px 5px;
+    align-self: start;
+  }
+  `
+  const reglasCssCheckBox = `
+  .list-checkboxes {
+    width: 20px;
+    height: 20px;
   }
   `
   style.appendChild(document.createTextNode(reglasCssDiv))
   style.appendChild(document.createTextNode(reglasCssTextArea))
   style.appendChild(document.createTextNode(reglasCssBtn))
+  style.appendChild(document.createTextNode(reglasCssCheckBox))
   document.head.appendChild(style)
 
+  const nuevoDiv = document.createElement('div')
+  const nuevoTextArea = document.createElement('textarea')
+  const nuevoBtn = document.createElement('button')
+  const nuevoCheckBox = document.createElement('input')
+  nuevoCheckBox.type = 'checkbox'
+
   nuevoDiv.classList.add('nuevo-div')
+  nuevoTextArea.classList.add('nuevo-textarea')
+  nuevoBtn.classList.add('list-btns')
+  nuevoCheckBox.classList.add('list-checkboxes')
+
   nuevoTextArea.textContent = document.getElementById('new-task').value
   setFocoNuevaTarea()
-
-  nuevoTextArea.classList.add('nuevo-textarea')
-
-  nuevoBtn.classList.add('list-btns')
   nuevoBtn.innerText = 'X'
 
   document.getElementById('task-list-wrapper').appendChild(nuevoDiv)
+  nuevoDiv.appendChild(nuevoCheckBox)
   nuevoDiv.appendChild(nuevoTextArea)
   nuevoDiv.appendChild(nuevoBtn)
+
+  nuevoCheckBox.addEventListener('change', function () {
+    if (this.checked) {
+      nuevoTextArea.disabled = true
+      nuevoTextArea.style.backgroundColor = 'lightgray'
+    } else {
+      nuevoTextArea.disabled = false
+      nuevoTextArea.style.backgroundColor = 'white'
+    }
+  })
 
   nuevoBtn.addEventListener('click', function () {
     this.parentElement.remove()
